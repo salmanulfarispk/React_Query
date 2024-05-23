9f1b939b9cc0a51fe2537f853e63394daaea00a1
Salmanul FarisPK <salmanulfarispk1405@gmail.com>
2024-05-23 22:31:30 +0530

    first commit

---
+++ react-query/src/App.jsx
@@ -0,0 +1,14 @@
+
+// "https://jsonplaceholder.typicode.com/todos/100"
+function App() {
+  
+
+  return (
+    <>
+      <h1 className="text-red-600 underline">hello</h1>
+      
+    </>
+  )
+}
+
+export default App
