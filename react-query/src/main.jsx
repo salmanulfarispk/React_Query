60d8c83181ea097095df972fd6d3e8293ed1f001
Salmanul FarisPK <salmanulfarispk1405@gmail.com>
2024-05-27 20:30:36 +0530

    finished!

--- react-query/src/main.jsx
+++ react-query/src/main.jsx
@@ -6,6 +6,7 @@ import {QueryClient,QueryClientProvider} from "@tanstack/react-query"
 
 
 const queryClient=new QueryClient({
+  
   defaultOptions: {queries: {gcTime: 10* (60 *1000)} }, //If the data is not accessed within the 10-minute gcTime window, it will be removed from the cache to free up memory.
 })      //in this page firstly defining providers
   
